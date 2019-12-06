948ae7182ce846a4cd32e216c28a61b3dcb3252f
ameer <ameerh@berkeley.edu>
2019-12-05 19:57:29 -0800

    remove support of negative value jobs

--- joblib/_ray.py
+++ joblib/_ray.py
@@ -18,12 +18,8 @@ class RayBackend(ParallelBackendBase, AutoBatchingMixin):
 
     def effective_n_jobs(self, n_jobs):
         """Determine the number of jobs/workers which are going to run in parallel"""
-        if n_jobs == 0:
-            raise ValueError('n_jobs == 0 in Parallel has no meaning')
-        elif n_jobs < 0:    
-            n_jobs = max(cpu_count() + 1 + n_jobs, 1)
-        else: #n_jobs > 0
-            n_jobs = n_jobs
+        if n_jobs <= 0:
+            raise ValueError('n_jobs <= 0 in ray Parallel has no meaning')
         return n_jobs
 
     def get_nested_backend(self):
